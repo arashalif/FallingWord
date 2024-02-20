2d4e015af07d32c0c5466d46b861d997c9373d3a
arash <aghelifar.arash@gmail.com>
2024-02-20 07:00:26 +0330

    feat(Stats): Implemented the Stats data class to represent game statistics
    This class includes properties such as question count, question played
    count, correct count, remaining count, score count, and game count,
    providing comprehensive tracking of game progress and performance.

---
+++ app/src/main/java/com/arashaghelifar/fallingword/domain/model/Stats.kt
@@ -0,0 +1,21 @@
+package com.arashaghelifar.fallingword.domain.model
+
+
+/**
+ * Represents domain class of Stats in the Falling Words game.
+ *
+ * @property questionCount The total number of questions.
+ * @property questionPlayedCount The number of questions played.
+ * @property correctCount The number of correct answers.
+ * @property remainingCount The number of remaining questions.
+ * @property scoreCount The total score.
+ * @property gameCount The number of games played.
+ */
+data class Stats(
+    val questionCount : Int = 0,
+    val questionPlayedCount : Int = 0,
+    val correctCount : Int = 0,
+    val remainingCount : Int = 0,
+    val scoreCount: Long = 0,
+    val gameCount:Int = 0
+)
